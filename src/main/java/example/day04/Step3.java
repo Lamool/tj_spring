830bc61893a380d8a53d23d0b2b7a68aa1de4fca
Lamool <gosu0522@naver.com>
2024-07-09 15:35:29 +0900

    240709

---
+++ src/main/java/example/day04/Step3.java
@@ -0,0 +1,17 @@
+package example.day04;
+
+public class Step3 {
+    public static void main(String[] args) {
+        String money = "123123123";
+
+        // 문제 : money 변수의 존재하는 문자열 금액의 천단위 쉼표를 넣기
+            // 천단위 쉼표가 포함된 형식으로 반환하는 함수 구현 하시오.
+
+        for (int i = money.length() - 1; i >= 0; i -= 3) {
+
+        }
+
+
+    }
+
+}
