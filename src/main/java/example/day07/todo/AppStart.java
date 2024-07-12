a1eaff6cdd4b99522c53bac351184bd3cea1d08e
Lamool <gosu0522@naver.com>
2024-07-12 18:26:22 +0900

    240712

---
+++ src/main/java/example/day07/todo/AppStart.java
@@ -0,0 +1,12 @@
+package example.day07.todo;
+
+public class AppStart {
+}
+
+
+/*
+    DB 설계 -> DTO -> Dao db 연동
+        처음부터 script하지 말고 샘플 데이터를 이용해서(Talend 사이트에서 테스트 진행. Response에 뜨면 성공) Controller -> dao 처리
+        HTML -> JS(AJAX) 처리
+
+*/
